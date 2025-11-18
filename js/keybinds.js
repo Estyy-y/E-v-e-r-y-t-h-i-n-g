8df88411a8a374761630b6d52b6316e1a226a6d1
Estyy <73704366+Estyy-y@users.noreply.github.com>
2025-11-18 23:13:45 +0000

    Game

---
+++ js/keybinds.js
@@ -0,0 +1,9 @@
+window.addEventListener('keydown', function(event) {
+  // event.key contains the key pressed, e.g. "a", "Enter"
+  if (event.key === 'm') {
+    // Do something when "a" is pressed
+    console.log('M key was pressed!');
+    for (let i = 0; i < player.thingamajigs.length; i++) {
+        player.thingamajigs[i].power();
+  }}
+});
---
+++ js/multiplier.js
@@ -0,0 +1,8 @@
+class Multiplier {
+    constructor (name, amount, target) {
+        this._name = name;
+        this._amount = amount;
+        // probably don't need this
+        this._target = target;
+    }
+}
