08fb4d9ffee0f68fff4f9ce14f6066016c24911e
Estyy <73704366+Estyy-y@users.noreply.github.com>
2025-11-18 23:13:19 +0000

    Oops

--- E v e r y t h i n g 2/js/keybinds.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('keydown', function(event) {
-  // event.key contains the key pressed, e.g. "a", "Enter"
-  if (event.key === 'm') {
-    // Do something when "a" is pressed
-    console.log('M key was pressed!');
-    for (let i = 0; i < player.thingamajigs.length; i++) {
-        player.thingamajigs[i].power();
-  }}
-});
--- E v e r y t h i n g 2/js/multiplier.js
+++
@@ -1,8 +0,0 @@
-class Multiplier {
-    constructor (name, amount, target) {
-        this._name = name;
-        this._amount = amount;
-        // probably don't need this
-        this._target = target;
-    }
-}
