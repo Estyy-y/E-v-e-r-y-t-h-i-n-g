4542416575b1788f87012a0587738cb0cbcecb11
Estyy <73704366+Estyy-y@users.noreply.github.com>
2025-11-18 23:12:34 +0000

    GAME

---
+++ E v e r y t h i n g 2/js/format.js
@@ -0,0 +1,21 @@
+function format(num) {
+  if (num.lt(1000)) {
+    const whole = num.floor().toNumber();
+    if (whole >= 100) {
+      // 3 whole digits
+      return whole.toString();
+    } else if (whole >= 10) {
+      // 2 digits, 1 decimal
+      return num.toFixed(1);
+    } else {
+      // 1 digit, 2 decimals
+      return num.toFixed(2);
+    }
+  } else {
+    // Scientific notation, x.xxey format
+    const exponent = Math.floor(num.log10());
+    // Use pow and div for mantissa calculation
+    const mantissa = num.div(num.constructor.pow(10, exponent)).toFixed(2);
+    return mantissa + "e" + exponent;
+  }
+}
---
+++ E v e r y t h i n g 2/js/player.js
@@ -0,0 +1,11 @@
+
+var player = {
+  things: new Decimal(1e10),
+  thingamajigs: [],
+  lastUpdate: Date.now()
+}
+
+for (let i = 0; i < 24; ++i) {
+  const tmg = new Thingamajig(i);
+  player.thingamajigs.push(tmg);
+}
